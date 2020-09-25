4611d44760c664e9bb1483057950fd04ef1e8b09
Sri Satya Harsha Karuturi <karuturiharsh@gmail.com>
2020-09-25 12:28:54 -0400

    Updated code

--- Lab-1/app.js
+++ Lab-1/app.js
@@ -1,4 +1,11 @@
 'use strict';
 
 console.log('Hello world');
-console.log('Harsha Karuturi);
+console.log('Harsha Karuturi');
+
+var firstname = 'Harsha';
+var lastname = 'Karuturi'
+
+var fullname = firstname+ "  " +lastname;
+console.log(fullname);
+
